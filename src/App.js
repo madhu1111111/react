21e742528dec9bd11217b41732a39d33b554e073
madhu1111111 <pawarmadhuri1819@gmail.com>
2025-06-01 14:39:32 +0530

    beach page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import logo from "./assets/coffee.jpg";
+import logo from "./assets/beach.jpg";
 
 function App() {
   return (
     <>
       <h3>My First Webpage</h3>
-      <h1>I Really love coffee</h1>
+      <h1>I Really love beach</h1>
       <p>Hello world!</p>
       <img src={logo} style={{ width: "500px" }} alt="" />
     </>
